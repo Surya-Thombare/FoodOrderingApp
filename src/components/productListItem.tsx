a7aa07d55f1dfd99b06159a2726729a3a03e21b7
Suryakant Thombre <suryathombre000@gmail.com>
2024-02-28 01:44:21 +0530

    typescript and paths configured

--- src/components/productListItem.tsx
+++ src/components/productListItem.tsx
@@ -1,25 +1,29 @@
 import { Image, StyleSheet } from "react-native";
-import { Text, View } from "@/src/components/Themed";
-import Colors from '@/src/constants/Colors';
+import { Text, View } from "@/components/Themed";
+import Colors from "@/constants/Colors";
+import { Product } from "../types";
 
-type pizza = {
-    id: number,
-    name: string,
-    image: string,
-    price: number,
-}
+export const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png";
 
-const ProductListItem = ({ product }) => {
+type ProductListItemProps = {
+  product: Product;
+};
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+      />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
   );
 };
 
-export default ProductListItem
+export default ProductListItem;
 
 const styles = StyleSheet.create({
   container: {
