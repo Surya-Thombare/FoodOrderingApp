620037e8eb97dd836d6116a3eea44bc12fa1e051
Suryakant Thombre <suryathombre000@gmail.com>
2024-03-02 14:20:44 +0530

    admin side p2

--- src/app/(admin)/menu/[id].tsx
+++ src/app/(admin)/menu/[id].tsx
@@ -45,33 +45,10 @@ const ProductDetailsScreen = (props: Props) => {
         source={{ uri: product.image || defaultPizzaImage }}
         style={styles.image}
       />
-      <Text>Select size</Text>
-      <View style={styles.sizes}>
-        {sizes.map((size) => (
-          <Pressable
-          onPress={() => setSelectedSize(size)}
-            style={[
-              styles.size,
-              {
-                backgroundColor: selectedSize === size ? "gainsboro" : "white",
-              },
-            ]}
-            key={size}
-          >
-            <Text
-              style={[
-                styles.sizeText,
-                { color: selectedSize === size ? "black" : "grey" },
-              ]}
-            >
-              {size}
-            </Text>
-          </Pressable>
-        ))}
-      </View>
+      
 
+      <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
-      <Button onPress={AddToCart} text="Add To Cart" />
     </View>
   );
 };
@@ -89,25 +66,10 @@ const styles = StyleSheet.create({
   price: {
     fontSize: 18,
     fontWeight: "bold",
-    marginTop: 'auto'
   },
-  sizes: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    marginVertical: 10,
-  },
-  size: {
-    backgroundColor: "gainsboro",
-    width: 50,
-    aspectRatio: 1,
-    borderRadius: 25,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  sizeText: {
+  title: {
     fontSize: 20,
-    fontWeight: "500",
-  },
+  }
 });
 
 export default ProductDetailsScreen;
