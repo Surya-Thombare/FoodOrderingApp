7c4505ef79093bd6f00cfb3d02b6409d2e902501
Surya-Thombare <suryathombre000@gmail.com>
2024-03-24 01:00:15 +0530

    ssh test

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,6 +6,7 @@ type ButtonProps = {
   text: string;
 } & React.ComponentPropsWithoutRef<typeof Pressable>;
 
+
 const Button = forwardRef<View | null, ButtonProps>(
   ({ text, ...pressableProps }, ref) => {
     return (
